08089cb68b2c56f9b9d91fb9a539101a86461e11
tobias20091708-ux <tobias20091708@gmail.com>
2026-08-17 20:08:00 +0200

    fix structlog level

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ structlog.configure(
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.dev.ConsoleRenderer(),
     ],
-    wrapper_class=structlog.make_filtering_bound_logger(settings.log_level),
+    wrapper_class=structlog.make_filtering_bound_logger(20),
 )
 log = structlog.get_logger()
 
